751d79e3ee516f001ed3c5e0a0715e2ffbdf11e0
caitevanish <cait.evan@gmail.com>
2022-02-10 14:14:56 -0800

    Fixed Like Button functionality in SinglePost.jsx. Started fixing styling on SinglePost. Simplified DummyPosts. Added ThumbsUp/Down icons to new images folder.

--- src/Components/SinglePost/SinglePost.jsx
+++ src/Components/SinglePost/SinglePost.jsx
@@ -1,28 +1,73 @@
 import "./SinglePost.css";
 import { useState } from 'react';
-
+import {Posts} from '../../DummyPosts'
+import thumbsUpOff from '../../Images/thumbsUpOff.png'
+import thumbsUpOn from '../../Images/thumbsUpOn.png'
+import thumbsDownOff from '../../Images/thumbsDownOff.png'
+import thumbsDownOn from '../../Images/thumbsDownOn.png'
 
 
 export default function SinglePost(props) { //props!! Know it, use it, love it.
-  console.log(props.post)
+  // console.log(props.post)
 
-  const [likeButton, setLikeButton] = useState(props.post);
-  const [isLiked, setIsLiked] = useState(false);
-  const [unlikeButton, setUnLikeButton] = useState(props.post);
-  const [isUnliked, setIsUnliked] = useState(false);
+
+  const [likeButton, setLikeButton] = useState(thumbsUpOff);
+  const [altLikeButton, setAltLikeButton] = useState(thumbsUpOn);
+  const [unLikeButton, setUnLikeButton] = useState(thumbsDownOff);
+  const [altUnlikeButton, setAltUnlikeButton] = useState(thumbsDownOn);
+
  
   function likeToggle(event){
-    setLikeButton(isLiked)
-    setIsLiked(!isLiked)
-
-
-
+    if (likeButton == thumbsUpOff){
+      setLikeButton(thumbsUpOn)
+    }
+    else{
+      setLikeButton(thumbsUpOff)
+    }
   }
 
   function unLikeToggle(event){
-    setUnLikeButton(isUnliked)
-    setIsUnliked(!isUnliked)
+    if (unLikeButton == thumbsDownOff){
+      setUnLikeButton(thumbsDownOn)
+    }
+    else{
+      setUnLikeButton(thumbsDownOff)
+    }
   }
+
+  return(
+    <div className="Post">
+      
+      <div className="postWrapper">
+
+        <div className="postTop">
+          
+          <div className="postTopLeft">
+            <span className="postUserName">{props.post.username}
+            </span>
+          </div>
+          
+          <div className="postTopRight">
+            <p className="postDate">{props.post.date}</p>
+          </div>
+        </div>
+
+        <div className="postMiddle">
+          <p className="postPost">{props.post.post}</p>
+        </div>
+
+        <div className="postBottom">
+          <div className="postBottomRight">
+            <img className="likeButton" src={likeButton} alt="Neutral thumbs up button" onClick={likeToggle}/> 
+            <img className="unLikeButton" src={unLikeButton} alt="Neutral thumbs down button" onClick={unLikeToggle}/>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  )
+}
+
 
   // useEffect(()=>{
   //   let tempSocialFeed = props.parentEntries.map(entry =>{
@@ -31,26 +76,3 @@ export default function SinglePost(props) { //props!! Know it, use it, love it.
   //   setLikeButton(tempSocialFeed);
   //   setUnlikeButton(tempSocialFeed);
   // }, [props.parentEntries])
-
-  return(
-    <div className="Post">
-      <div className="PostWrapper">
-        <div className="PostTop">
-          <div className="postTopLeft">
-            <p className="postUserName">User Name</p>
-          </div>
-          <div className="postTopRight">
-            <p className="postDate">Post Date</p>
-          </div>
-        </div>
-        <div className="PostBottom">
-          <div className="PostBottomRight">
-            <img className="likeButton" src="" alt="" onClick={likeToggle}/>
-            <img className="unLikeButton" src="" alt="" onClick={unLikeToggle}/>
-          </div>
-        </div>
-      </div>
-    </div>
-
-  )
-}
--- src/DummyPosts.js
+++ src/DummyPosts.js
@@ -1,51 +1,32 @@
-export const Users =[
-{
-  id: 1,
-  username: "Name",
-},
-{
-  id: 2,
-  username: "Name",
-},
-{
-  id: 3,
-  username: "Name",
-},
-{
-  id: 4,
-  username: "Name",
-}
-]
-
 export const Posts = [
 {
   id: 1,
   date: "02-03-2022",
-  userId: 3,
-  post: "Something here",
+  username: "James Potter",
+  post: "I'm obsessed with my wife and my best friends a dirty dog!",
 },
 {
   id: 2,
   date: "02-03-2022",
-  userId: 4,
+  username: "Name",
   post: "Something here",
 },
 {
   id: 3,
   date: "02-05-2022",
-  userId: 2,
+  username: "Name",
   post: "Something here",
 },
 {
   id: 4,
   date: "02-07-2022",
-  userId: 1,
+  username: "Name",
   post: "Something here",
 },
 {
   id: 5,
   date: "02-12-2022",
-  userId: 4,
+  username: "Name",
   post: "Something here",
 },
 
